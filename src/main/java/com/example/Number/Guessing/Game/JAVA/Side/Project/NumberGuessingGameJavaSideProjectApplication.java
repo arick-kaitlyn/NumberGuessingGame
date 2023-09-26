c173e4a539966698d57164a14bde3dbf973b0015
Kaitlyn Arick <Arick.Kaitlyn@gmail.com>
2023-09-25 20:16:11 -0400

    Updated

---
+++ src/main/java/com/example/Number/Guessing/Game/JAVA/Side/Project/NumberGuessingGameJavaSideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Number.Guessing.Game.JAVA.Side.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberGuessingGameJavaSideProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberGuessingGameJavaSideProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Number/Guessing/Game/JAVA/Side/Project/NumberGuessingGameJavaSideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Number.Guessing.Game.JAVA.Side.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberGuessingGameJavaSideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
